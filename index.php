f5d298d4f80f9a9c0881809efeeff75d0d291f5d
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-05 18:29:28 +0800

    another download PHP is added 3

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "Hello World! You Mother Fucker keke";
 
 
 file_put_contents("download.jpg", 
-    file_get_contents("https://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg")
+    file_get_contents("https://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg"));
 ?>
