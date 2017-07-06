13031cd8cc1f6de2451a40ab6d6847a264b48211
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 14:37:34 +0800

    Php download Version 1.9.1

--- index.php
+++ index.php
@@ -5,8 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Download Code Version 1.9";
-header('Content-Type: image/jpg');
-header('Content-Disposition: attachment; filename= "Image.jpg"');
+echo "Download Code Version 1.9.1";
+
 file_put_contents("Image.jpg", fopen("https://lh3.googleusercontent.com/VDb2ZriRXTVuzZqW4rBv56dP-zrBnvUHbAhK0PL_7cgRP09DmDtaV3UIj-p4ZjaqbfxF8y5W8ze6zWY=w1366-h662-rw", 'r'));
 ?>
