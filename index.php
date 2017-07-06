b3576beedb15f531b4b38e97da73f4d57df16321
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 11:17:15 +0800

    Php download Version 1.1

--- index.php
+++ index.php
@@ -4,5 +4,5 @@
 echo "Download Code Version 1.0";
 
 
-file_put_contents("images.jpg",file_get_contents("https://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg"));
+copy('https://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', 'Downloads/Image.jpg');
 ?>
