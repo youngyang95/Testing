bfcbc6e6e451c9973b3ad1e2d9e73f8acc02fbbc
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 11:34:30 +0800

    Php download Version 1.3

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 
-echo "Download Code Version 1.2";
+echo "Download Code Version 1.3";
 
 
-copy('https://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', '/Downloads/Image.jpg');
+copy('https://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', 'Image.jpg');
 ?>
