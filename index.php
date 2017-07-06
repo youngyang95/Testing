8af273581514330360c324d5be43aded5b02017c
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 15:36:12 +0800

    Php download Version 1.9.5

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
 
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
+echo "Download Code Version 1.5.1";
 
-echo "Download Code Version 1.9.4";
-header('Location: http://drive.google.com/open?id=0BzYOCiHocV0fMENJNERvbnQzRkk');
-header('Content-Type: image/jpg');
-header('Content-Disposition: attachment; filename= "Image.jpg"');
-
+copy('http://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', './Image.jpg');
+if(!@copy('http://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', './Image.jpg'))
+{
+    $errors= error_get_last();
+    echo "COPY ERROR: ".$errors['type'];
+    echo "<br />\n".$errors['message'];
+} else {
+    echo "File copied from remote!";
+}
 ?>
