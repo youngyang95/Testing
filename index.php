e3edc858925d7f6b0a12d72d3052c4e55e2e2b03
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 17:42:40 +0800

    Php download Version 2.2

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 
-echo "Download Code Version 2.1";
+echo "Download Code Version 2.2";
 
 copy('https://standardhong.blob.core.windows.net/container1/download.jpg', './Image.jpg');
 echo copy('https://standardhong.blob.core.windows.net/container1/download.jpg', './Image.jpg');
-if(!@copy('https://standardhong.blob.core.windows.net/container1/download.jpg.jpg', './Image.jpg'))
+if(!@copy('https://standardhong.blob.core.windows.net/container1/download.jpg', './Image.jpg'))
 {
     $errors= error_get_last();
     echo "COPY ERROR: ".$errors['type'];
