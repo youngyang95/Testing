ed7c5714495a6bebfd97c45088c6b06054b34836
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 14:59:41 +0800

    Php download Version 1.9.4

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Download Code Version 1.9.2";
+echo "Download Code Version 1.9.4";
+header('Location: http://drive.google.com/open?id=0BzYOCiHocV0fMENJNERvbnQzRkk');
+header('Content-Type: image/jpg');
+header('Content-Disposition: attachment; filename= "Image.jpg"');
 
-file_put_contents("Image.jpg", fopen("http://drive.google.com/open?id=0BzYOCiHocV0fMENJNERvbnQzRkk", 'r'));
 ?>
