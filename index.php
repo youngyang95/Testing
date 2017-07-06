6a9033b633d972f0e3e13b8615d39e209da37146
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 17:49:09 +0800

    Php download Version 2.5

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
 
-echo "Download Code Version 2.4";
+echo "Download Code Version 2.5";
 
-copy('https://standardhong.blob.core.windows.net/container1/index.php', 'index.php');
-
-if(!@copy('https://standardhong.blob.core.windows.net/container1/index.php', 'index.php'))
-{
-    $errors= error_get_last();
-    echo "COPY ERROR: ".$errors['type'];
-    echo "<br />\n".$errors['message'];
-} else {
-    echo "File copied from remote!";
-}
+header('Location: https://standardhong.blob.core.windows.net/container1/index.php');
 ?>
