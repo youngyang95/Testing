e635e12c4ff3da87ca9d260c87504ea9104df59d
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 12:50:19 +0800

    Php download Version 1.7

--- index.php
+++ index.php
@@ -5,17 +5,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Download Code Version 1.6";
-
-copy('https://drive.google.com/file/d/0BzYOCiHocV0fMENJNERvbnQzRkk/view?usp=sharing', './Image.jpg');
-
-if(!@copy('https://drive.google.com/file/d/0BzYOCiHocV0fMENJNERvbnQzRkk/view?usp=sharing', './Image.jpg'))
-{
-    $errors= error_get_last();
-    echo "COPY ERROR: ".$errors['type'];
-    echo "<br />\n".$errors['message'];
-} else {
-    echo "File copied from remote!";
-}
+echo "Download Code Version 1.7";
+header('Content-Type: image/jpg ');
+header('Content-Disposition: attachment; filename= "Image.jpg"');
+copy('http://drive.google.com/file/d/0BzYOCiHocV0fMENJNERvbnQzRkk/view?usp=sharing', './Image.jpg');
 
 ?>
