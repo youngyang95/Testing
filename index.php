ac4a76ed7c53c7931690cabd5d4846c0615e402b
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 13:03:53 +0800

    Php download Version 1.8

--- index.php
+++ index.php
@@ -5,9 +5,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-echo "Download Code Version 1.7";
-header('Content-Type: image/jpg ');
+echo "Download Code Version 1.8";
+header('Location: http://drive.google.com/file/d/0BzYOCiHocV0fMENJNERvbnQzRkk/view?usp=sharing');
 header('Content-Disposition: attachment; filename= "Image.jpg"');
-copy('http://drive.google.com/file/d/0BzYOCiHocV0fMENJNERvbnQzRkk/view?usp=sharing', './Image.jpg');
 
 ?>
