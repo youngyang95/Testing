a663958e1a095c1d7550e16ffbe9ff95c3d95ec0
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 14:39:03 +0800

    Php download Version 1.9.2

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ error_reporting(E_ALL);
 
 echo "Download Code Version 1.9.1";
 
-file_put_contents("Image.jpg", fopen("https://lh3.googleusercontent.com/VDb2ZriRXTVuzZqW4rBv56dP-zrBnvUHbAhK0PL_7cgRP09DmDtaV3UIj-p4ZjaqbfxF8y5W8ze6zWY=w1366-h662-rw", 'r'));
+file_put_contents("Image.jpg", fopen("http://lh3.googleusercontent.com/VDb2ZriRXTVuzZqW4rBv56dP-zrBnvUHbAhK0PL_7cgRP09DmDtaV3UIj-p4ZjaqbfxF8y5W8ze6zWY=w1366-h662-rw", 'r'));
 ?>
