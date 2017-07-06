4b68c4b4b3fc7cd377d5c3be888e62f5a52bbba9
DESKTOP-S0NSL47\YoungYang <pzy95@hotmail.com>
2017-07-06 12:01:20 +0800

    Php download Version 1.5

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
 <?php
 
 
-echo "Download Code Version 1.4";
+echo "Download Code Version 1.5";
 
 
-copy('http://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', 'Image.jpg');
+copy('http://raw.githubusercontent.com/youngyang95/Testing/master/download.jpg', './Image.jpg');
+
+if(!@copy('http://someserver.com/somefile.zip','./somefile.zip'))
+{
+    $errors= error_get_last();
+    echo "COPY ERROR: ".$errors['type'];
+    echo "<br />\n".$errors['message'];
+} else {
+    echo "File copied from remote!";
+}
+
 ?>
